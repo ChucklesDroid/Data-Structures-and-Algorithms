025e3000e72fcbbecc78e34efb69dee9b47a7eaa
Aakarsh MJ <mj.akarsh@gmail.com>
2026-08-13 23:21:48 +0200

    fix: improved code efficiency for comparisions

--- Module 1: Arrays and Hashing/Contains Duplicates/cpp/brute-force.cpp
+++ Module 1: Arrays and Hashing/Contains Duplicates/cpp/brute-force.cpp
@@ -12,20 +12,31 @@ public:
   bool containsDuplicate(std::vector<int> &nums) {
     for (int i = 0; i < nums.size(); i++) {
       for (int j = i; j < nums.size(); j++) {
-        if (nums.at(i) == nums.at(j)) {
+        // at() method performs bound safety checks at each call
+        // which increase overhead in the foll. snippet.
+        // if (nums.at(i) == nums.at(j)) {
+
+        if (nums[i] == nums[j]) {
           return true;
         }
       }
     }
+
     return false;
   }
 };
 
 int main() {
     Solution solution;
-    /*This is wrong initialization, the comma operator just returns the rightmost operand so */
-    /*in this case, its just returning 6.*/
-    /*std::vector<int> test = std::vector<int>((1, 2, 3, 2, 4, 5, 6));*/
+    /*
+      This is wrong initialization, when you put expressions seperated by commas inside single 
+      paranthesis like (a,b,c) the comma operator is triggered which just returns the rightmost 
+      operand so in this case, its just returning 6.
+      std::vector<int> test = std::vector<int>((1, 2, 3, 2, 4, 5, 6));
+
+      The above becomes:
+      std::vector<int> test = std::vector<int>(6); // which is a vector with 6 zeros
+    */
 
     /*"{}" is used for initializing containers*/
     std::vector<int> nums = {1, 2, 3, 1};
