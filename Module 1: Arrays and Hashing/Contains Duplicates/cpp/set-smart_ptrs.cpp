21c159938a4336467d98d231d41bc388c08d60b0
Aakarsh MJ <mj.akarsh@gmail.com>
2026-08-18 00:58:21 +0200

    fix: use smart pointers in set-smart_ptrs.cpp

--- Module 1: Arrays and Hashing/Contains Duplicates/cpp/set-smart_ptrs.cpp
+++ Module 1: Arrays and Hashing/Contains Duplicates/cpp/set-smart_ptrs.cpp
@@ -4,22 +4,55 @@
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 
+// #include <iostream>
+// #include <memory>
+// #include <unordered_set>
+// #include <vector>
+//
+// class Solution {
+// public:
+//   bool containsDuplicate(std::vector<int> &nums) {
+//     auto st =
+//         std::make_unique<std::unordered_set<int>>(nums.begin(), nums.end());
+//     return st->size() < nums.size();
+//   }
+// };
+//
+// int main() {
+//   Solution solution;
+//   auto nums = std::vector<int>{2, 3, 4, 2};
+//   std::cout << solution.containsDuplicate(nums);
+// }
+
+
+#include <initializer_list>
 #include <iostream>
-#include <memory>
-#include <unordered_set>
+#include <set>
 #include <vector>
+#include <memory>
+
+using namespace std;
 
 class Solution {
 public:
-  bool containsDuplicate(std::vector<int> &nums) {
-    auto st =
-        std::make_unique<std::unordered_set<int>>(nums.begin(), nums.end());
-    return st->size() < nums.size();
-  }
+    bool containsDuplicate(weak_ptr<vector<int>> nums) {
+        int lstLen, vecLen;
+        if (auto sp = nums.lock()) {
+            vecLen = sp->size();
+            set<int> lst(sp->begin(), sp->end());
+            lstLen = lst.size();
+        }
+        return vecLen != lstLen;
+    }
 };
 
 int main() {
-  Solution solution;
-  auto nums = std::vector<int>{2, 3, 4, 2};
-  std::cout << solution.containsDuplicate(nums);
+    Solution sol;
+    shared_ptr<vector<int>> nums = make_shared<vector<int>>(initializer_list<int>{1, 2, 3, 3}); 
+    if(sol.containsDuplicate(nums)) {
+        cout << "Contains Duplicates\n";
+    } else {
+        cout << "Does Not Contain Duplicates\n";
+    }
+    return 0;
 }
